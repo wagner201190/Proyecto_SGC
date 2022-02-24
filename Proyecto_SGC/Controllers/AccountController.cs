f64d9664e46c408ee44e20d835b8ea2f2eaa8339
wagner201190 <90158672+wagner201190@users.noreply.github.com>
2022-02-24 12:17:33 -0600

    ajustes daniela

--- Proyecto_SGC/Controllers/AccountController.cs
+++ Proyecto_SGC/Controllers/AccountController.cs
@@ -200,8 +200,8 @@ namespace Proyecto_SGC.Controllers
             if (ModelState.IsValid)
             {
                 var user = await UserManager.FindByNameAsync(model.Email);
-                if (user == null //|| !(await UserManager.IsEmailConfirmedAsync(user.Id)))
-                ){
+                if (user == null || !(await UserManager.IsEmailConfirmedAsync(user.Id)))
+                {
                     // Don't reveal that the user does not exist or is not confirmed
                     return View("ForgotPasswordConfirmation");
                 }
